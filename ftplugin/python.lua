467299ce89e3af7c4e86aa48e1c0f124f9d4f30b
Yi Ming <ofseed@foxmail.com>
2023-12-24 12:58:27 +0800

    fix: avoid running before installation

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -1,3 +1,9 @@
 local dance = require "dance"
 
+---@diagnostic disable-next-line: param-type-mismatch
+if not vim.uv.fs_stat(dance.opts.server.entry) then
+  vim.notify("No Python language server found", vim.log.levels.WARN)
+  return
+end
+
 dance.start()
